834d5f04577cc3a78e5e4f66b750a8e02de3aeee
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-11 21:55:35 +0900

    Add: CRUD2 Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud2',
+        'USER': 'root',
+        'PASSWORD': '0421',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-@)@8$065o5ef41d=5xvtk+*vs$s-4z5)rob=qolurcx909vm6-'
