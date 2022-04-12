b839099f64883a3f76a792bbbbe9daec8546d2dc
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-12 10:34:18 +0900

    Add: owners database tables

---
+++ owners/models.py
@@ -0,0 +1,16 @@
+from pyexpat import model
+from django.db import models
+
+class Owner(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=300)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'owners'
+
+class Dog(models.Model):
+    Owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+# Create your models here.
